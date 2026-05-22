fecc8d715ef414a61818b71cfd5ffb4a145d502b
ElNgatia <ngatiadyte@gmail.com>
2026-05-22 10:43:18 +0300

    ci: update set app version

--- .github/workflows/scripts/set_app_version.sh
+++ .github/workflows/scripts/set_app_version.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
 function set_app_version() {
-  echo 'Setting release version'
-  build_number=$(echo $(grep "^version:" pubspec.yaml | sed 's/.*+\(.*\)/\1/' | awk '{print $1 + 1}'))
-  version=$(echo "$TAG_NAME" | cut -d 'v' -f2)
-  echo "APP_VERSION=$version" >>$GITHUB_OUTPUT
-  echo "BUILD_NUMBER=$build_number" >>$GITHUB_OUTPUT
-  echo "RELEASE_VERSION=$version+$build_number" >>$GITHUB_OUTPUT
+  echo "Setting release version"
+
+  build_number=$(
+    grep "^version:" pubspec.yaml |
+    sed 's/.*+\(.*\)/\1/' |
+    awk '{print $1 + 1}'
+  )
+
+  version="${TAG_NAME#v}"
+
+  if [[ "$TAG_NAME" == *"-hotfix."* ]]; then
+    release_version=$(echo "$version" | sed -E 's/-hotfix\.[0-9]+//')
+
+    app_version=$(echo "$release_version" | cut -d '+' -f1)
+
+    echo "APP_VERSION=$app_version" >> "$GITHUB_OUTPUT"
+    echo "BUILD_NUMBER=$build_number" >> "$GITHUB_OUTPUT"
+    echo "RELEASE_VERSION=$release_version" >> "$GITHUB_OUTPUT"
+  else
+    echo "APP_VERSION=$version" >> "$GITHUB_OUTPUT"
+    echo "BUILD_NUMBER=$build_number" >> "$GITHUB_OUTPUT"
+    echo "RELEASE_VERSION=${version}+${build_number}" >> "$GITHUB_OUTPUT"
+  fi
 }
 
 set_app_version
